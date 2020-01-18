d2625f3c55102e4f55865d17ec3e82f11b69cab8
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-01-18 22:38:07 +0100

    :lipstick: display output in place of the button

--- static/main.js
+++ static/main.js
@@ -1,4 +1,8 @@
 ((form, textarea, button, output, category, confidence) => {
+  textarea.addEventListener('input', () => {
+    form.classList.remove('predicted');
+  });
+
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     button.disabled = true;
@@ -8,6 +12,7 @@
     fetch(`/prediction?text=${encodeURIComponent(text)}`)
       .then(response => response.json())
       .then((result) => {
+        form.classList.add('predicted');
         output.className = result.category;
         category.innerHTML = result.category;
         confidence.innerHTML = `${Math.round(result.confidence * 100)}%`;
